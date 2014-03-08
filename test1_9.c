0b8fa23b3ca6ec633582d53e0932223a7835bfc6
root <root@ubuntu.(none)>
2014-03-07 23:17:31 -0800

    add test1_9.c

---
+++ test1_9.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	int line_1,line_2,line_3;
+	printf("Please input 3 number as triangle's 3 line!\n");
+	scanf("%d,%d,%d",&line_1,&line_2,&line_3);
+	if(line_1+line_2 > line_3 && line_1+line_3 > line_2 && line_2+line_3 > line_1)
+	{
+		if(line_1*line_1+line_2*line_2 = line_3*line_3 || line_1*line_1+line_3*line_3 = line_2*line_2 || line_3*line_3+line_2*line_2 = line_1*line_1)
+			printf("Yes!\n");
+		else
+			printf("No!\n");
+	}
+	else
+		printf("Not a triangle!\n");
+	return 0;
+}
