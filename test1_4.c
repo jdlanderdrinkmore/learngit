cdc56a4a3f2da1d0fd9e24fc66a149e7e5070b34
root <root@ubuntu.(none)>
2014-03-07 23:24:47 -0800

    just a modefied

--- test1_4.c
+++ test1_4.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 #include<math.h>
-
+#define pi 3.1415926
 
 int main()
 {
-	float  angle;
+	float  rad,angle;
 	printf("Please input an angle number between 0-360!\n");
 	scanf("%f",&angle);
-	printf("The sin(%f)=%.4f,cos(%f)=%.4f\n",angle,sin(angle),angle,cos(angle));
+	rad = angle/180*pi;
+	printf("The sin(%f)=%.4f,cos(%f)=%.4f\n",rad,sin(rad),rad,cos(rad));
 	return 0;
 }
--- test1_9.c
+++ test1_9.c
@@ -7,7 +7,7 @@ int main()
 	scanf("%d,%d,%d",&line_1,&line_2,&line_3);
 	if(line_1+line_2 > line_3 && line_1+line_3 > line_2 && line_2+line_3 > line_1)
 	{
-		if(line_1*line_1+line_2*line_2 = line_3*line_3 || line_1*line_1+line_3*line_3 = line_2*line_2 || line_3*line_3+line_2*line_2 = line_1*line_1)
+		if(line_1*line_1+line_2*line_2 == line_3*line_3 || line_1*line_1+line_3*line_3 == line_2*line_2 || line_3*line_3+line_2*line_2 == line_1*line_1)
 			printf("Yes!\n");
 		else
 			printf("No!\n");
